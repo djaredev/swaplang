0b99e0d1bb6f00a9095e2fa76dd83f5fc63a8298
Diego Jared <djjaredev@gmail.com>
2025-10-10 12:00:27 -0600

    (frontend) Add copy to clipboard
    (frontend) Clean code

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -52,6 +52,10 @@
 			targetText = data['translation'];
 		}, 3000);
 	};
+
+	const copyToClipboard = async () => {
+		await navigator.clipboard.writeText(targetText);
+	};
 </script>
 
 <div class="container">
@@ -102,13 +106,20 @@
 			<div class="output-section">
 				<div class="text-output" id="translatedText">{targetText}</div>
 				<div class="action-buttons">
-					<button class="icon-btn" id="copyBtn" title="Copy translation">
-						<svg viewBox="0 0 24 24">
-							<path
-								d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"
-							/>
-						</svg>
-					</button>
+					{#if targetText}
+						<button
+							class="icon-btn"
+							id="copyBtn"
+							title="Copy translation"
+							onclick={copyToClipboard}
+						>
+							<svg viewBox="0 0 24 24">
+								<path
+									d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"
+								/>
+							</svg>
+						</button>
+					{/if}
 				</div>
 			</div>
 		</div>
