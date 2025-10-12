f97db01256a9d6cc61ba587ce78f084b601eaa6e
Diego Jared <djjaredev@gmail.com>
2025-10-12 08:37:57 -0600

    (backend) Add app settings

---
+++ backend/swaplang/config/settings.py
@@ -0,0 +1,18 @@
+from pydantic import EmailStr, Field, SecretStr, computed_field
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_DIALECT: str = "sqlite"
+    DB_DRIVER: str = ""
+    SUPERUSER_USERNAME: str = Field(min_length=2, max_length=255)
+    SUPERUSER_EMAIL: EmailStr = Field(max_length=255)
+    SUPERUSER_PASSWORD: SecretStr = Field(min_length=8, max_length=40)
+
+    @computed_field
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"{self.DB_DIALECT}{self.DB_DRIVER}:///swaplang.db"
+
+
+settings = Settings()  # type: ignore
