afb282f98cc73646f6cfc22d465971ce86fed7fa
Diego Jared <djjaredev@gmail.com>
2025-10-13 12:38:06 -0600

    (backend) Create routes for auth
    Add login endpoint

---
+++ backend/swaplang/api/routes/auth.py
@@ -0,0 +1,37 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from swaplang.auth import create_access_token
+from swaplang.database import SessionDep
+from swaplang.models import UserPublic
+from swaplang.services import auth_service as service
+
+router = APIRouter()
+
+
+@router.post("/login", response_model=UserPublic)
+async def login(
+    session: SessionDep,
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    response: Response,
+):
+    user = service.login(
+        session=session, username=form_data.username, password=form_data.password
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect username or password",
+        )
+
+    access_token = create_access_token(user.id)
+    response.set_cookie(
+        key="swaplang_token",
+        value=access_token,
+        httponly=True,
+        secure=False,
+    )
+
+    return user
