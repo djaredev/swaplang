eb1a8d9e5d3f5deff569093e306e4702b6a008d9
Diego Jared <djjaredev@gmail.com>
2025-10-12 20:46:43 -0600

    (backend) Update init file
    Add new imports

--- backend/swaplang/auth/__init__.py
+++ backend/swaplang/auth/__init__.py
@@ -1,1 +1,2 @@
-from swaplang.auth.password import get_password_hash
+from swaplang.auth.password import get_password_hash, verify_password
+from swaplang.auth.token import create_access_token, decode_token
