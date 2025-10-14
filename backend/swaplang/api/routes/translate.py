6f8a5bcfb1f1bd2a3d7eb4139187fe7d325ebcea
Diego Jared <djjaredev@gmail.com>
2025-10-14 16:06:38 -0600

    (backend) Update endpoint to translate
    * Get and validate available languages against the database
    * Handle None values

--- backend/swaplang/api/routes/translate.py
+++ backend/swaplang/api/routes/translate.py
@@ -1,16 +1,38 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 
 from swaplang.auth.user import AuthUserDep
+from swaplang.database import SessionDep
+from swaplang.models import Translated
 from swaplang.translator import translate
+from swaplang.services import language_service
 
 router = APIRouter()
 
 
-@router.post("/translate")
+@router.post("/translate", response_model=Translated)
 async def swap_lang(
-    user: AuthUserDep, text: str, source_language: str, target_language: str
+    user: AuthUserDep,
+    session: SessionDep,
+    text: str,
+    source_language: str,
+    target_language: str,
 ):
-    translation = translate(
-        text=text, source_language=source_language, target_language=target_language
-    )
-    return {"translation": translation}
+    sl = language_service.get_lang(session, source_language)
+    tl = language_service.get_lang(session, target_language)
+    if not sl:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Language '{source_language}' not available",
+        )
+    if not tl:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Language '{target_language}' not available",
+        )
+    translation = translate(text=text, source_language=sl.name, target_language=tl.name)
+    if not translation:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error while translating",
+        )
+    return Translated(lang=target_language, text=translation)
