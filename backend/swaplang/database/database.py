cce85455d06b736f1b00b330bb9da77cfaa0e1bb
Diego Jared <djjaredev@gmail.com>
2025-10-14 16:14:30 -0600

    (backend) Update init db: Register default languages when initializing the database

--- backend/swaplang/database/database.py
+++ backend/swaplang/database/database.py
@@ -3,8 +3,8 @@ from typing import Annotated, Generator
 from fastapi import Depends
 from sqlmodel import SQLModel, Session, create_engine, select
 from swaplang.config import settings
-from swaplang.models import User
-from swaplang.services import user_service as service
+from swaplang.models import User, Language
+from swaplang.services import language_service, user_service
 
 logger = logging.getLogger("database")
 
@@ -28,10 +28,16 @@ def init_db():
     create_db_and_tables()
     with Session(engine) as session:
         user = session.exec(select(User).limit(1)).first()
+        langs = session.exec(select(Language).limit(1)).first()
         if not user:
             logger.info("Creating superuser...")
-            service.create_superuser(session)
+            user_service.create_superuser(session)
             logger.info("Superuser created.")
         else:
             logger.info("Superuser already exists.")
+        if not langs:
+            logger.info("Creating default languages")
+            language_service.create_default_langs(session)
+        else:
+            logger.info("Default langauges already exists")
     logger.info("Database initialized.")
--- backend/swaplang/services/language_service.py
+++ backend/swaplang/services/language_service.py
@@ -5,3 +5,19 @@ from swaplang.models import Language
 
 def get_lang(session: Session, id: str) -> Language | None:
     return session.exec(select(Language).where(Language.id == id)).first()
+
+
+def create_default_langs(session: Session):
+    session.add(Language(id="en", name="English"))
+    session.add(Language(id="es", name="Spanish"))
+    session.add(Language(id="fr", name="French"))
+    session.add(Language(id="de", name="German"))
+    session.add(Language(id="it", name="Italian"))
+    session.add(Language(id="pt", name="Portuguese"))
+    session.add(Language(id="ru", name="Russian"))
+    session.add(Language(id="zh", name="Chinese"))
+    session.add(Language(id="ja", name="Japanese"))
+    session.add(Language(id="ko", name="Korean"))
+    session.add(Language(id="ar", name="Arab"))
+    session.add(Language(id="hi", name="Hindi"))
+    session.commit()
