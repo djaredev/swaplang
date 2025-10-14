729af3598eec897449ceeb401426fdd83d0d54d3
Diego Jared <djjaredev@gmail.com>
2025-10-13 22:09:35 -0600

    (backend) Add authentication to the endpoint

--- backend/swaplang/api/routes/translate.py
+++ backend/swaplang/api/routes/translate.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter
 
+from swaplang.auth.user import AuthUserDep
 from swaplang.translator import translate
 
 router = APIRouter()
 
 
-@router.post("/api/translate")
-async def swap_lang(text: str, source_language: str, target_language: str):
+@router.post("/translate")
+async def swap_lang(
+    user: AuthUserDep, text: str, source_language: str, target_language: str
+):
     translation = translate(
         text=text, source_language=source_language, target_language=target_language
     )
