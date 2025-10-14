bf841388b2a8aa9d1dbc9a4e4793d61495beea5a
Diego Jared <djjaredev@gmail.com>
2025-10-14 13:23:16 -0600

    (backend) Add models for Translations

---
+++ backend/swaplang/models/translation.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+from sqlmodel import Field, SQLModel
+
+
+class TranslationBase(SQLModel):
+    source_text: str = Field(min_length=1)
+    target_text: str = Field(min_length=1)
+    source_lang: str = Field(max_length=2, foreign_key="language.id")
+    target_lang: str = Field(min_length=2, foreign_key="language.id")
+
+
+class TranslationUpdate(SQLModel):
+    source_text: str | None = Field(default=None, min_length=1)
+    target_text: str | None = Field(default=None, min_length=1)
+    source_lang: str | None = Field(default=None)
+    target_lang: str | None = Field(default=None)
+
+
+class TranslationPublic(TranslationBase):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    create_at: datetime = Field(default_factory=lambda: datetime.now())
+    update_at: datetime = Field(default_factory=lambda: datetime.now())
+
+
+class TranslationCreate(TranslationBase): ...
+
+
+class Translation(TranslationPublic, table=True):
+    user_id: UUID = Field(foreign_key="user.id")
