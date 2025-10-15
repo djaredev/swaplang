ffa734879e4fc8a5053ec61f607624475a6f0214
Diego Jared <djjaredev@gmail.com>
2025-10-15 13:57:42 -0600

    (backend) Update service: add logic to get translations using cursor-based pagination

--- backend/swaplang/services/translate_service.py
+++ backend/swaplang/services/translate_service.py
@@ -1,6 +1,8 @@
+from typing import Sequence
 from sqlmodel import Session, and_, col, or_, select
 
 from swaplang.models import Translation, User, Direction, Cursor
+from swaplang.utils.cursor import decode_cursor, encode_cursor
 
 
 def create_translate(
@@ -21,3 +23,45 @@ def create_translate(
         )
     )
     session.commit()
+
+
+def get_translations(
+    user: User,
+    session: Session,
+    encoded_cursor: str | None,
+    limit: int,
+    direction: Direction,
+) -> tuple[Sequence[Translation], str | None]:
+    statement = select(Translation).where(Translation.user_id == user.id)
+    if encoded_cursor:
+        cursor = decode_cursor(encoded_cursor)
+        if direction == Direction.NEXT:
+            statement = statement.where(
+                or_(
+                    Translation.created_at < cursor.created_at,
+                    and_(
+                        Translation.created_at == cursor.created_at,
+                        Translation.id < cursor.id,
+                    ),
+                )
+            ).order_by(col(Translation.created_at).desc(), col(Translation.id).desc())
+        else:
+            statement = statement.where(
+                or_(
+                    Translation.created_at > cursor.created_at,
+                    and_(
+                        Translation.created_at == cursor.created_at,
+                        Translation.id > cursor.id,
+                    ),
+                )
+            ).order_by(col(Translation.created_at).asc(), col(Translation.id).asc())
+    else:
+        statement = statement.order_by(
+            col(Translation.created_at).desc(), col(Translation.id).desc()
+        )
+    translations = session.exec(statement.limit(limit)).all()
+    if not translations:
+        return [], None
+    return translations, encode_cursor(
+        Cursor(id=translations[-1].id, created_at=translations[-1].created_at)
+    )
