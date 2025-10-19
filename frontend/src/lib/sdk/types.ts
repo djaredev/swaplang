8e9d5999bff67d2d938d01d0b6ad9bc5dbc292b3
Diego Jared <djjaredev@gmail.com>
2025-10-18 19:09:16 -0600

    (frontend) Add types for the translate endpoint data

--- frontend/src/lib/sdk/types.ts
+++ frontend/src/lib/sdk/types.ts
@@ -7,3 +7,14 @@ export type UserPublic = {
 	username: string;
 	email: string;
 };
+
+export type Translate = {
+	text: string;
+	source_language: string;
+	target_language: string;
+};
+
+export type Translated = {
+	lang: string;
+	text: string;
+};
