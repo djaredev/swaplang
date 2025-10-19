66d70d70dc5da78eb164aa5e5f70158ee5fbb06f
Diego Jared <djjaredev@gmail.com>
2025-10-18 19:05:49 -0600

    (frontend) Update apiUrl: add params

--- frontend/src/lib/sdk/utils.ts
+++ frontend/src/lib/sdk/utils.ts
@@ -1,5 +1,12 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
-export const apiUrl = (route: string): string => {
-	return `${API_URL}/api${route}`;
+export const apiUrl = (
+	route: string,
+	params?: string | string[][] | Record<string, string>
+): string => {
+	let url = `${API_URL}/api${route}`;
+	if (params) {
+		url += `?${new URLSearchParams(params)}`;
+	}
+	return url;
 };
