a520a8b201a78afc026bbd59461fe13b2c2212e7
Diego Jared <djjaredev@gmail.com>
2025-12-11 14:21:39 -0600

    (frontend) Create component to load data lazily

---
+++ frontend/src/lib/components/LazyLoading.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let { loadData } = $props();
+	let lazyLoading: HTMLElement;
+
+	const observer = new IntersectionObserver(
+		(entries) => {
+			if (entries[0].isIntersecting) {
+				loadData();
+			}
+		},
+		{
+			root: null,
+			rootMargin: '0px',
+			threshold: 0.5
+		}
+	);
+
+	onMount(() => {
+		observer.observe(lazyLoading);
+	});
+</script>
+
+<div class="lazy-loading" bind:this={lazyLoading}></div>
+
+<style>
+	.lazy-loading {
+		/* position: absolute; */
+		width: 100px;
+		height: 200px;
+		/* visibility: hidden; */
+		background: red;
+	}
+</style>
