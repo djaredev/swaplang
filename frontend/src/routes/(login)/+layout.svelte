95acd61af593ebc100cf47e6fcbab2b0c5c1b752
Diego Jared <djjaredev@gmail.com>
2025-12-11 19:01:48 -0600

    (frontend) Create login route
    Add initial style for login page

---
+++ frontend/src/routes/(login)/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import '../../app.css';
+
+	let { children } = $props();
+</script>
+
+{@render children()}
+
+<style>
+	:global(html) {
+		height: 100%;
+		width: 100%;
+	}
+
+	:global(body) {
+		display: flex;
+		height: 100%;
+		width: 100%;
+		margin: 0;
+		padding: 0;
+		/* background: #11111b; */
+		box-sizing: border-box;
+	}
+</style>
