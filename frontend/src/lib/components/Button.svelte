1b6c35386849cafd0b54438e6c6e4b2b6e13b8f6
Diego Jared <djjaredev@gmail.com>
2025-12-11 19:07:18 -0600

    (frontend) Create Buttom component

---
+++ frontend/src/lib/components/Button.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	let { children, ...restProps } = $props();
+</script>
+
+<button class="button" {...restProps}>
+	{@render children()}
+</button>
+
+<style>
+	* {
+		box-sizing: border-box;
+	}
+	.button {
+		background: linear-gradient(135deg, #4285f4, #34a853);
+		border: none;
+		color: #11111b;
+		cursor: pointer;
+		margin-top: 10px;
+		padding: 10px;
+		border-radius: 10px;
+		font-size: 16px;
+
+		&:hover {
+			background-color: #bac2de;
+		}
+
+		&:active {
+			transform: scale(0.98);
+		}
+
+		&:disabled {
+			opacity: 0.5;
+			cursor: not-allowed;
+
+			&:hover {
+				background-color: #cdd6f4;
+			}
+
+			&:active {
+				transform: scale(1);
+			}
+		}
+	}
+</style>
