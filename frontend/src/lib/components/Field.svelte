aadba9917d2ec9e23525c63971f678624d45ae81
Diego Jared <djjaredev@gmail.com>
2025-12-11 19:07:00 -0600

    (frontend) Create Field component

---
+++ frontend/src/lib/components/Field.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	let { class: className = '', value = $bindable(), ...restProps } = $props();
+</script>
+
+<input class={['field', className]} bind:value {...restProps} />
+
+<style>
+	* {
+		box-sizing: border-box;
+	}
+	.field {
+		width: 100%;
+		height: 45px;
+		background: white;
+		color: #cdd6f4;
+		border-radius: 8px;
+		border: 1px solid #e8eaed;
+		outline: none;
+		color: #3c4043;
+		font-size: 16px;
+		padding: 10px;
+	}
+
+	.field:focus {
+		border-color: #8ab4f8;
+		box-shadow: 0 0 0 4px #d2e3fc;
+	}
+</style>
