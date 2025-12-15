b09fc2e7e833c5be79015b5a12a5fc2fa715fa69
Diego Jared <djjaredev@gmail.com>
2025-12-15 17:34:30 -0600

    (backend) Add model

--- backend/swaplang/models/language.py
+++ backend/swaplang/models/language.py
@@ -4,9 +4,15 @@ from sqlmodel import Field, SQLModel
 class LanguageBase(SQLModel):
     id: str = Field(max_length=2, primary_key=True)
     name: str = Field(min_length=1, max_length=20)
+    enabled: bool = Field(default=True)
 
 
 class LanguagePublic(LanguageBase): ...
 
 
 class Language(LanguageBase, table=True): ...
+
+
+class LanguageUpdate(SQLModel):
+    id: str = Field(max_length=2)
+    enabled: bool = Field(default=None)
