5927f1323cff63de163e22146f00fcf285629eb2
Diego Jared <djjaredev@gmail.com>
2025-12-15 17:35:31 -0600

    (backend) Add service to update enabled languages

--- backend/swaplang/services/settings_service.py
+++ backend/swaplang/services/settings_service.py
@@ -1,9 +1,16 @@
 from sqlmodel import Session, select
 
-from swaplang.models.language import Language
+from swaplang.models.language import Language, LanguageUpdate
 
 
 def get_available_languages(session: Session):
     return session.exec(select(Language)).all()
 
 
+def update_enabled_languages(session: Session, languages: list[LanguageUpdate]):
+    for lang in languages:
+        db_lang = session.get(Language, lang.id)
+        if db_lang:
+            db_lang.enabled = lang.enabled
+            session.add(db_lang)
+    session.commit()
