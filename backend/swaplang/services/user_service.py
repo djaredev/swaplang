ba2441f0aa9c5a7b52a8ec600bc37f424f09331e
Diego Jared <djjaredev@gmail.com>
2025-12-15 22:39:41 -0600

    (backend) Add service to update user password

--- backend/swaplang/services/user_service.py
+++ backend/swaplang/services/user_service.py
@@ -1,8 +1,8 @@
-from sqlmodel import Session
-from swaplang.auth.password import get_password_hash
+from fastapi import HTTPException
 from sqlmodel import Session, select
+from swaplang.auth.password import get_password_hash, verify_password
 from swaplang.config import settings
-from swaplang.models import User
+from swaplang.models import User, UpdatePassword
 
 
 def create_superuser(session: Session):
@@ -28,3 +28,18 @@ def get_user_by_username(username: str, session: Session) -> User | None:
 def get_user_by_email(email: str, session: Session) -> User | None:
     db_user = session.exec(select(User).where(User.email == email)).first()
     return db_user
+
+
+def update_password(user: User, passwords: UpdatePassword, session: Session):
+    if not verify_password(passwords.current_password, user.hashed_password):
+        raise HTTPException(status_code=400, detail="Incorrect password")
+    if passwords.current_password == passwords.new_password:
+        raise HTTPException(
+            status_code=400,
+            detail="New password must be different from the current one.",
+        )
+    user.hashed_password = get_password_hash(passwords.new_password)
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
