0a01dd34fb8113fae0b234dc2966468e7d65d867
Diego Jared <djjaredev@gmail.com>
2025-12-16 15:15:16 -0600

    (frontend) Fetch enabled languages on page load

--- frontend/src/routes/(main)/+page.ts
+++ frontend/src/routes/(main)/+page.ts
@@ -1,6 +1,9 @@
 import type { PageLoad } from './$types';
 import { auth } from '$lib/utils/auth.svelte';
+import { getAvailableLanguages } from '$lib/sdk/sdk';
 
 export const load: PageLoad = async () => {
 	await auth();
+	const langs = await getAvailableLanguages(true);
+	return { langs };
 };
