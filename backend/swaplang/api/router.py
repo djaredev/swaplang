f98d247c874377fdfc3e2416d1d9852df77fe08f
Diego Jared <djjaredev@gmail.com>
2025-12-15 22:43:08 -0600

    (backend) Create user route

--- backend/swaplang/api/router.py
+++ backend/swaplang/api/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from swaplang.api.routes import auth, translate, settings
+from swaplang.api.routes import auth, translate, settings, users
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router)
 api_router.include_router(translate.router)
 api_router.include_router(settings.router)
+api_router.include_router(users.router)
---
+++ backend/swaplang/api/routes/users.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter, status
+
+
+router = APIRouter(prefix="/users", tags=["users"])
