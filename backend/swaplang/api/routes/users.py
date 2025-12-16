a959d518c818c419546aa22e261b4b702c8a3e1b
Diego Jared <djjaredev@gmail.com>
2025-12-15 22:44:56 -0600

    (backend) Add endpoint to update user password

--- backend/swaplang/api/routes/users.py
+++ backend/swaplang/api/routes/users.py
@@ -1,4 +1,16 @@
 from fastapi import APIRouter, status
 
+from swaplang.auth.user import AuthUserDep
+from swaplang.database.database import SessionDep
+from swaplang.models.user import UpdatePassword, UserPublic, UserUpdate
+from swaplang.services import user_service
+
 
 router = APIRouter(prefix="/users", tags=["users"])
+
+
+@router.patch("/me/password", status_code=status.HTTP_204_NO_CONTENT)
+async def update_password_me(
+    user: AuthUserDep, session: SessionDep, passwords: UpdatePassword
+):
+    user_service.update_password(user, passwords, session)
