c6f027b1ebb28f36441d21a95496d5e8fc6b6342
Diego Jared <djjaredev@gmail.com>
2025-12-15 20:09:27 -0600

    (frontend) Fetch system prompt on page load

--- frontend/src/routes/(main)/settings/translator/+page.ts
+++ frontend/src/routes/(main)/settings/translator/+page.ts
@@ -1,11 +1,12 @@
 import type { PageLoad } from './$types';
 import { auth } from '$lib/utils/auth.svelte';
-import { getAvailableLanguages, getAvailableModels } from '$lib/sdk/sdk';
+import { getAvailableLanguages, getAvailableModels, getSystemPrompt } from '$lib/sdk/sdk';
 
 export const load: PageLoad = async () => {
 	await auth();
 	const availablelanguages = await getAvailableLanguages();
 	const availableModels = await getAvailableModels();
+	const systemPrompt = await getSystemPrompt();
 	console.log(availablelanguages);
-	return { availablelanguages, availableModels };
+	return { availablelanguages, availableModels, systemPrompt };
 };
