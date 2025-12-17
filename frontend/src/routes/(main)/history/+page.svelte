4ac3fa52bd304091e82678877f3ada85cf694bc5
Diego Jared <djjaredev@gmail.com>
2025-12-17 07:24:51 -0600

    (frontend) Add title

--- frontend/src/routes/(main)/history/+page.svelte
+++ frontend/src/routes/(main)/history/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	import History from '$lib/components/History.svelte';
+	let { data } = $props();
 </script>
 
-<History />
+<h1>Last translations</h1>
+<History translations={data.translations} />
+
+<style>
+	h1 {
+		opacity: 10%;
+	}
+</style>
