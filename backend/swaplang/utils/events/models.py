d92eb7257490bc6abe404658daa6fcc3f1dd1dab
Diego Jared <djjaredev@gmail.com>
2025-12-18 18:39:11 -0600

    (backend) Create MessageEvent model

---
+++ backend/swaplang/utils/events/models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, model_serializer
+
+
+class MessageEvent(BaseModel):
+    data: str | dict
+    id: int | None = None
+    event: str | None = None
+    retry: int | None = None
+
+    @model_serializer(mode="plain")
+    def serialize_model(self) -> str:
+        parts = []
+        if self.id is not None:
+            parts.append(f"id: {self.id}")
+        if self.event is not None:
+            parts.append(f"event: {self.event}")
+        if self.retry is not None:
+            parts.append(f"retry: {self.retry}")
+        if isinstance(self.data, dict):
+            parts.append(str(self.data))
+        else:
+            for line in self.data.splitlines():
+                parts.append(f"data: {line}")
+        return "\n".join(parts) + "\n\n"
