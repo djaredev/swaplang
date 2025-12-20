b18e9488efd2be8ac726ee25b5e7f6280e87a653
Diego Jared <djjaredev@gmail.com>
2025-12-19 18:46:34 -0600

    (backend) Add: load model

--- backend/swaplang/translator/model.py
+++ backend/swaplang/translator/model.py
@@ -2,6 +2,7 @@ from llama_cpp import Llama
 from huggingface_hub import hf_hub_download
 from swaplang.config import settings
 from swaplang.utils.events import TqdmToEvent
+import asyncio
 
 
 _llm = None
@@ -14,6 +15,18 @@ def _download_model():
         local_dir=settings.MODELS_DIR,
         tqdm_class=TqdmToEvent,
     )
+
+
+def _load_model() -> Llama:
+    if not settings.MODEL_PATH.exists():
+        _download_model()
+    return Llama(
+        model_path=str(settings.MODEL_PATH),
+        n_gpu_layers=-1,
+        n_ctx=2048,
+    )
+
+
 async def get_model() -> Llama:
     global _llm
     if _llm is None:
