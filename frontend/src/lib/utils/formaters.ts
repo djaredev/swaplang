b22e2717d85addfe6698b4744dddeab5e151d346
Diego Jared <djjaredev@gmail.com>
2025-12-20 11:53:42 -0600

    (frontend) Add util to format seconds into hours, minutes and seconds

--- frontend/src/lib/utils/formaters.ts
+++ frontend/src/lib/utils/formaters.ts
@@ -8,3 +8,15 @@ export function bytesToFormattedSize(bytes: number): string {
 
 	return `${(bytes / MB).toFixed(2)} MB`;
 }
+
+export function secondsToTimeFormat(totalSeconds: number): string {
+	const hours = Math.floor(totalSeconds / 3600);
+	const minutes = Math.floor((totalSeconds % 3600) / 60);
+	const seconds = totalSeconds % 60;
+
+	if (hours > 0) {
+		return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+	}
+
+	return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
