55b50fcf96091b1ee37db7dc5eb451eee54dbe9a
Diego Jared <djjaredev@gmail.com>
2025-12-19 18:37:38 -0600

    (backend) Add: download model from hf hub
    (backend) Fix commit

--- backend/swaplang/translator/model.py
+++ backend/swaplang/translator/model.py
@@ -1,10 +1,20 @@
 from llama_cpp import Llama
+from huggingface_hub import hf_hub_download
+from swaplang.config import settings
+from swaplang.utils.events import TqdmToEvent
 
 
 _llm = None
 
 
-def get_model() -> Llama:
+def _download_model():
+    hf_hub_download(
+        repo_id=settings.HF_HUB_REPO_ID,
+        filename=settings.DEFAULT_MODEL,
+        local_dir=settings.MODELS_DIR,
+        tqdm_class=TqdmToEvent,
+    )
+async def get_model() -> Llama:
     global _llm
     if _llm is None:
         _llm = Llama(model_path="gemma-3-1b-it-Q8_0.gguf", n_gpu_layers=-1, n_ctx=2048)
