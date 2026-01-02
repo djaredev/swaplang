295e0d014b03650d19c91da25cf86f09f685e98c
Diego Jared <djjaredev@gmail.com>
2026-01-01 23:08:21 -0600

    (frontend) Create custom error page

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { CircleXIcon } from 'lucide-svelte';
+</script>
+
+<div class="container">
+	<div class="error">
+		<CircleXIcon size={50} />
+		<h1>{page.status}: {page.error?.message}</h1>
+	</div>
+</div>
+
+<style>
+	* {
+		box-sizing: border-box;
+	}
+
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+		color: #d20f39;
+		padding: 20px;
+	}
+	.error {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 20px;
+		width: 600px;
+		padding: 20px;
+		background: white;
+		border: 1px solid #c2cad3;
+		border-radius: 10px;
+		overflow: hidden;
+		@media (width <= 600px) {
+			& {
+				flex-direction: column;
+				width: 100%;
+				height: auto;
+			}
+		}
+	}
+</style>
